27c9e9c0574623b443d025f9e75ec04742f85f50
Rahul ray <54851912+rahulray1406@users.noreply.github.com>
2020-03-24 17:22:37 +0530

    Create FIFO.c

---
+++ FIFO.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define ReferenceLength 100
+
+typedef struct
+{
+    int *PG_Number1;
+    int CountforElem;	
+}Framing;
+
+int ForReference, SecondFrame;
+
+MF;
+
+int *StringPointer;
+void reference();
+
+void frameinit();
+
+void reference2();
+
+void frameinit2();
+
+int FIFO();
+
+int LRU();
